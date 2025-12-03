ebe453c0cb2c536b3855bd6d7f333cbaffb89ffb
Merlin HENRY <merlin.henry@edu.univ-fcomte.fr>
2025-12-03 14:02:22 +0100

    fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+
+const LIGHTBOX = document.querySelector("#lightbox");
+
+const LIGHTBOX_IMG = document.querySelector("#lightbox img");
+
+LIGHTBOX_IMG?.addEventListener("click", () => LIGHTBOX.close());
+document.body.addEventListener("click", (evt) => {
+  if (!evt.target.matches("[data-full-img]")) return;
+  LIGHTBOX_IMG.src = evt.target.dataset.fullImg;
+  LIGHTBOX.showModal();
+});
